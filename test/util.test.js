04e7b5bbe31e46eef998b7e7b1660bf56c8966c6
adi ofry <aofry@yahoo.com>
2016-02-09 18:29:15 +0200

    add simple test

---
+++ test/util.test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var logger = require('../lib/logFactory.js');
+
+
+describe('util', function () {
+
+    //beforeEach(function () {
+
+    //});
+
+    describe('logger', function () {
+        it('should check that logger exist', function () {
+            assert(logger != null, 'logger exist');
+
+        });
+
+    })
+});
