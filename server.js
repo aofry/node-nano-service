7a41c758f6ce1c22bd25cbf6422cc0af28b0df3c
adi ofry <aofry@yahoo.com>
2016-02-14 20:49:14 +0200

    refactored server to have an object of params, and to send this object back as json

--- server.js
+++ server.js
@@ -2,27 +2,37 @@ var express = require('express');
 var cache = require('./lib/redisApi.js');
 
 var app = express();
-var counter = 0;
+
+var data = {
+    redisMasterLocation: process.env.REDIS_MASTER,
+    counter: 0,
+    serverBootTime: new Date(),
+    serverListenPort: process.env.PORT || 3000,
+    dumbServiceUrl: process.env.DUMB_SERVICE_URL
+};
 
 app.get('/', function (req, res) {
-    var redisLocation = process.env.REDIS_MASTER;
-    console.log('Sending result for: ' + counter);
-    console.log('redis location: ' + redisLocation);
+
+    console.log('Sending result for: ' + data.counter);
+    console.log('redis location: ' + data.redisMasterLocation);
 
     if (cache) {
         cache.get("time", function(err, cacheReturnRes) {
             //console.log("got result: " + cacheReturnRes);
             if (cacheReturnRes) {
-                res.send('Redis Location = ' + redisLocation + ' date from redis = ' + cacheReturnRes + '. Call num: ' + counter++);
+                data.date = cacheReturnRes;
+                res.send(data);
             } else
-                res.send('Redis Location = ' + redisLocation + '. Call num: ' + counter++);
+                res.send(data);
             cache.set("time", new Date() + '');
         });
 
     } else
-        res.send('Redis Location = ' + redisLocation + '. Call num: ' + counter++);
+        res.send(data);
+
+    data.counter++;
 });
 
-app.listen(3000, function () {
+app.listen(data.serverListenPort, function () {
     console.log('Example app2 listening on port 3000!');
 });
